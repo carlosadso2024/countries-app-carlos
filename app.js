b04f1cac44e3de404035fa53f7e93181a0b171c7
Jalzate257 <malzatej257@gmail.com>
2025-04-29 20:26:13 -0500

    creacion del contenedor para mostrar todas las tarjetas de los paises inicialmente

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 import { countries } from './countries.js'
 
+const content = document.getElementById('cardContent');
+
+countries.forEach((card) => {
+    const div = document.createElement("div")
+    div.classList.add("country-card")
+    div.innerHTML = `
+            
+        <h2>${card.country}</h2>
+        <p>Capital: ${card.capital}</p>
+        <p>Poblacion: ${card.population}</p>`;
+        
+    content.appendChild(div)
+});
+
+
+
 console.log(countries)
